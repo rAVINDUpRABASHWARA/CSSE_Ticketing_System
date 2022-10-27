951f9b40dcae81cef015fa51f731dde67ba86d02
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-10-27 23:03:27 +0530

    2022-10-27 11:03 PM

---
+++ client/src/components/Footer/footerComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footerComponent = () => {
+    return (
+        <div>
+            <h1>Footer Component</h1>
+        </div>
+    )
+}
+
+export default footerComponent
---
+++ client/src/screens/registration/registrationScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const registrationScreen = () => {
+    return (
+        <div>
+            <h1>Registration Page</h1>
+        </div>
+    )
+}
+
+export default registrationScreen
